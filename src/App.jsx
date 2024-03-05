08ebb9252600e74e6add1574dd8f4ef10f49cd4a
kumaranMba <kumarandinesh0411@gmail.com>
2024-03-05 23:11:08 +0530

    creating a post

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,43 @@ import React from "react";
 
 function App(){
   return(
-    <div>
-      <p>Hello World</p>
+    <div className="container">
+      <div>
+      <h2 className="text-center py-5">My todo</h2>
+      </div>
+      <form method="get">
+        <div className="row">
+        <div className="col-4">
+          <input type="text" className="form-control" placeholder="Todo Name" name ="tname"/>
+          </div>
+           <div className="col-5">
+           <input type="text" className="form-control" placeholder="Todo Description" name="tDescription" />
+         </div>
+         <div className="col-3">
+         <button type="submit" class="btn btn-primary">AddTodo</button>
+         </div>
+      </div>
+    </form>
+    <section className="py-5">
+      <div className="container border">
+        <div className="row">
+          <div className="col">
+            <h3>My Todos</h3>
+            <form name ="status" className="">
+            <label for="statusFilter" >Status Filter:
+              <select name ="status"  >
+              <option value="All">All</option>
+              <option value="completed">completed</option>
+              <option value=" not completed"> not completed</option>
+              </select>
+              </label>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
     </div>
+    
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+let MyTodos=[{id:1,
+              name:"office Task - 1",
+              Description:"this is the description for My First Task",
+                },{
+              id:2,
+              name:"office Task - 2",
+              Description:"this is the description for My Second Task",
+                },{
+                id:3,
+                name:"office Task - 3",
+                Description:"this is the description for My Third Task",
+                }]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App /> );
